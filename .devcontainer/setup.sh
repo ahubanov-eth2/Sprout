2ae3264ba13dae80bbccd88416214499c6e9d089
Aleksandar Hubanov <ahubanov@student.ethz.ch>
2025-10-25 15:21:33 +0300

    test: change scripy

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -e
 
-git config --global url.\"https://x-access-token:${GITHUB_TOKEN}@github.com/\".insteadOf \"git@github.com:\"
+git config --global url."https://x-access-token:${GITHUB_TOKEN}@github.com/".insteadOf "git@github.com:"
 git submodule update --init --recursive
 export DYNAMIC_REPO_PATH=${DYNAMIC_REPO_PATH:-mattermost/mattermost}
 git clone https://x-access-token:${GITHUB_TOKEN}@github.com/${DYNAMIC_REPO_PATH}.git data/project-repository
+
+if [ ! -d "data/project-repository" ]; then
+    echo "Error: Failed to clone repository into data/project-repository."
+    exit 1
+fi
+
 cd data/project-repository
-git checkout PARENT_COMMIT=${PARENT_COMMIT:-632b231283}
-cd \"webapp\" // TODO: make this generic 
-npm install // TODO: make this generic 
+git checkout ${PARENT_COMMIT:-632b231283}
+cd webapp # TODO: make this generic
+npm install --ignore-scripts # TODO: make this generic
